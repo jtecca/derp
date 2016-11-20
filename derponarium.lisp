61d2537cbe5f80b6e6ffc159c14570f2a9edb4c6
dptd <dptdescribe@gmail.com>
2016-11-20 20:03:29 +0100

    start derp in a separate thread

--- derponarium.lisp
+++ derponarium.lisp
@@ -16,29 +16,34 @@
                (slot-value derp 'derp::channel)))
 
 (defun add-derp (derp)
-  (setf *derps* (cons (cons
-                       (prehash derp)
-                       (derponarium::make-derp-thread :derp-instance derp
-                                                      :thread nil
-                                                      :running nil))
-                      *derps*)))
+  (if (typep derp 'derp::derp)
+      (setf *derps* (cons (cons
+                           (prehash derp)
+                           (derponarium::make-derp-thread :derp-instance derp
+                                                          :thread nil
+                                                          :running nil))
+                          *derps*))
+      (error "Parameter must be an instance of a derp class.")))
 
 (defun find-derp (derp)
-  (if (stringp derp)
+  (if (stringp derp)o
       (assoc derp *derps* :test 'string=)
-      (error "Must be a string.")))
+      (error "Parameter must be a string.")))
 
 (defun get-derp (derp)
   (if (stringp derp)
       (derp-thread-derp-instance (cdr (find-derp derp)))
-      (error "Must be a string.")))
+      (error "Parameter must be a string.")))
 
-;; (defun start-derp (derp)
-;;   (if (stringp derp)
-;;       (progn
-;;         (bt:make-thread (derp:start-derping (get-derp derp)))
-;;         (derp-thread-thread (find-derp derp))
-;;       (error "Must be a string.")))
+(defun start-derp (derp)
+  (if (stringp derp)
+      (if (find-derp derp)
+          (progn
+            (setf (derp-thread-thread (cdr (find-derp derp))) (bt:make-thread (lambda ()
+                                                                          (derp:start-derping (get-derp derp)))))
+            (setf (derp-thread-running (cdr (find-derp derp))) t))
+          (error (format nil "Cannot find derp named: ~A." derp)))
+      (error "Must be a string.")))
 
 (defun stop-derp (derp)
   ())
